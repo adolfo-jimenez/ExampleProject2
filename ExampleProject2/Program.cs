f11faaea44e7cb57fe6ac41ab7e7ab99af893ba9
CrazyAl369 <dlfbonds@gmail.com>
2022-07-02 16:16:15 -0700

    Code Simplified.

---
+++ ExampleProject2/Program.cs
@@ -0,0 +1,15 @@
+﻿//This program was created for general testing purposes.
+
+string introLine = "Please enter a number:";
+Console.WriteLine(introLine);
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int threshold = 6;
+
+if (num1 == threshold)
+    Console.WriteLine("Warning! You have reached the limit!");
+if (num1 < threshold)
+    Console.WriteLine("Safe Conditions Met.");
+if (num1 > threshold)
+    Console.WriteLine("Too Much, passed threshold.");
+    
